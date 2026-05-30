72c45744a09e6585c3414c41bc00fa3433bf5b0b
Byte Chanokchon <byte.chanokchon.won@gmail.com>
2026-05-30 22:57:53 +0700

    lect: if

---
+++ sources/004_more_control_flow_tool.py
@@ -0,0 +1,11 @@
+# More control flow tool
+
+print("Example: if statement")
+x: int = 0
+if x < 0:
+    print("Number should more than zero")
+elif x > 100:
+    print("Number should less than 100")
+else:
+    print("Save successful")
+
